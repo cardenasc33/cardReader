ed23bcc147c49f9ce4dfd61d01bc1826137ba4c5
Christian Cardenas <christiancardenas@10-31-48-124.ece.uic.edu>
2019-02-12 17:55:54 -0600

    Second commit

---
+++ cardSwipe.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+    
+    </head>
+    <body>
+        <input type="text" onkeydown="inputDetect()">
+        <div id="txtCardReader">
+        </div>
+    </body>
+    <script>
+        function inputDetect(){
+            alert("Card has been swiped");
+        }
+    </script>
+
+
+</html>
