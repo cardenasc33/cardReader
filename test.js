49bf23d737b373729cd841a8e2ae48e3f0ccf2cb
Christian Cardenas <christiancardenas@10-31-48-124.ece.uic.edu>
2019-02-21 16:16:16 -0600

    7th commit 2-21-2019

--- test.js
+++ test.js
@@ -1,5 +1,19 @@
+var fs = require("fs");
+        var sampleObject = {
+                            uin: "123123",
+                            studentInfo:{
+                            firstName: "Phil",
+                            middleName: "",
+                            lastName: "Beltran",
+                            rsvpStatus: "Yes"
+                        }
+                    };
 
-$.getJSON( "test1.json" , function( json) {
-    console.log("JSON Data recieved, name is " + json.firstName);
-    console.log("UIN is: " + json.uin);
-});
+   var fs = require('fs');  //gets the file system module for reading and writing files
+   fs.readFile('./test1.json', 'utf8', function (err, data) {  //provide callback with data field
+    var json = JSON.parse(data); //parses the data into json array string
+    json.push(sampleObject);  //appends the contents of the object to the array
+    fs.writeFile('./test1.json', JSON.stringify(json), function(err, result) {  //transforms JSON back to the string, write file and save file
+       if(err) console.log('error', err); //Print out error if not successful
+     });
+   });
