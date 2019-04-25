2c66e9bd20557985f387729529c7e468a9b202d9
Christian A. Cardenas <43822034+cardenasc33@users.noreply.github.com>
2019-04-25 14:59:59 -0500

    Update csvUpload.js

--- express_react_root/client/src/csvUpload.js
+++ express_react_root/client/src/csvUpload.js
@@ -1,3 +1,5 @@
+import React, { Component } from 'react';
+
 var fs = require('fs');
 
 var csv = require('fast-csv');
@@ -10,3 +12,41 @@ fs.createReadStream('datasample.csv')
     .on('end', function(data){
         console.log('Read finished');
     })
+class csvUpload extends Component{
+
+
+
+
+    render() {
+        return (
+          
+         <div>
+          <button onClick={this.handleClick}>
+            Upload
+          </button>
+  
+          <form ref='uploadForm' 
+        id='uploadForm' 
+        action='/upload' 
+        method='post' 
+        encType="multipart/form-data">
+          <input type="file" name="sampleFile" />
+          <input type='submit' value='Upload!' />
+      </form>  
+  
+  
+          <div id="db_contents">
+              {this.state.success}
+  
+          </div>
+         
+              
+              </div>
+   
+  
+        );
+      }
+
+    }
+
+    export default csvUpload;
